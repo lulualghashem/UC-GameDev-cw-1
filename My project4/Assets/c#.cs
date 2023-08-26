2930b7cf670150372d168e97d3c11189f0e81f6f
lulualghashem <142631126+lulualghashem@users.noreply.github.com>
2023-08-26 19:56:06 +0300

    Done

---
+++ My project4/Assets/c#.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string heroname = "Lulu";
+        int age = 14;
+        bool ishero = true;
+        float speed = 20f;
+
+        print(" My Name is Lulu "+ heroname + " And My Age is 14 "+ age);
+
+
+
+
+        int x1 = 2 + 2;
+
+        print (x1);
+
+      
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
